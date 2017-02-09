54e895f8a98d8ea8ad23e95ad712d8e283de2f0e
kubedav <davesade.42@gmail.com>
2017-02-09 15:59:16 +0100

    Removed column limit.

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ var flag = 1;
 
 var params = {
     TableName: table,                       /* The DynamoDB table to connect to */
-    ProjectionExpression: column,           /* The column(s) we want to be returned */
-    FilterExpression: "fight = :vlajka",    /* Search term; in this case return rows whose Ansi column value equals 'fight' */
+//    ProjectionExpression: column,           /* The column(s) we want to be returned - in case you want ONLY some columns - we want them all, full object! */
+    FilterExpression: "fightflag = :flag",    /* Search term; in this case return rows whose Ansi column value equals 'fightflag' */
     ExpressionAttributeValues: {
-         ":vlajka": flag                     /* Search value 'fight' substituted into the search term where :vlajka is found */
+         ":flag": flag                     /* Search value 'flag' substituted into the search term where :vlajka is found */
     }
 };
 
@@ -31,15 +31,16 @@ function onScan(err, data) {
     if (err) {
         console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
     } else {
-        // print all fight wanting users
+        // print all fight-wanting users
+        console.log(data);
         console.log("Scan succeeded.");
         data.Items.forEach(function(data) {
            console.log(
-                data.user + ": ",
+                data.name + ": ",
                 data.fightflag);
         });
 
-        // continue scanning if we have more movies, because
+        // continue scanning if we have more users, because
         // scan can retrieve a maximum of 1MB of data
         if (typeof data.LastEvaluatedKey != "undefined") {
             console.log("Scanning for more...");
