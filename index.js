be515f9fd24a9ee682ad83ad28f1fb0987448a73
kubedav <davesade.42@gmail.com>
2017-02-10 09:32:49 +0100

    Added first randomisation function.

--- index.js
+++ index.js
@@ -24,14 +24,25 @@ var params = {
     }
 };
 
+
 console.log("Scanning main table.");
+
+
 docClient.scan(params, onScan);
+
+
 
 function onScan(err, data) {
     if (err) {
         console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
     } else {
         // print all fight-wanting users
+
+        // randomise array
+
+        shuffle.array(err, data);
+        // This is almost certainly wrong!
+
         console.log(data);
         console.log("Scan succeeded.");
         data.Items.forEach(function(data) {
@@ -50,6 +61,23 @@ function onScan(err, data) {
     }
 }
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 };
